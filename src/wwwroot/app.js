c44cc64de8e7c58e3753b863113f62cf224051fb
Alexander.Daily <alexander.daily@ics-gmbh.de>
2021-05-18 17:25:40 +0200

    Front end for File IO

--- src/wwwroot/app.js
+++ src/wwwroot/app.js
@@ -1,13 +1,29 @@
 function send() {
-	let url = new URL("/api", location.origin);
-	let rating = document.getElementById("data").value;
-	
+	const rating = document.getElementById("data").value;
+	const url = new URL("/api", location.origin);
 	url.searchParams.append("data", rating);
 
 	fetch(url)
 		.then(response => response.text())
 		.then(text => {
 			document.getElementById("prediction").innerHTML = text;
+			document.getElementsByClassName("prediction")[0].style.visibility = "visible";
 		});
+}
+
+function send_score(event) {
+	const text = document.getElementById("data").value;
+	const score = event.target.value;
+
+	const request = {
+		text: text,
+		score: score
+	};
+	const url = new URL("/api", location.origin);
+
+	text.value = "";
+	document.getElementsByClassName("prediction")[0].style.visibility = "hidden";
+
+	fetch(url, { method: "POST", body: JSON.stringify(request), headers: { 'Content-Type': 'application/json'}  })
 }
 	
